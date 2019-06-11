bc1def50a219779d8dda453bdc664ee5fe512935
hudvin <hudvin@gmail.com>
2019-06-11 17:31:06 +0300

    wip

--- docker_demo.py
+++ docker_demo.py
@@ -35,16 +35,20 @@ default_args = {
 dag = DAG(
     'docker_sample', default_args=default_args, schedule_interval=timedelta(minutes=10))
 
-t1 = BashOperator(
-    task_id='print_date',
-    bash_command='date',
+t1 = DockerOperator(api_version='1.19',
+    command='/bin/sleep 30',
+    image='centos:latest',
+    network_mode='bridge',
+    task_id='docker_op_tester',
     dag=dag)
 
-t2 = BashOperator(
-    task_id='sleep',
-    bash_command='sleep 5',
-    retries=3,
+t2 = DockerOperator(api_version='1.19',
+    command='/bin/sleep 30',
+    image='centos:latest',
+    network_mode='bridge',
+    task_id='docker_op_tester',
     dag=dag)
+
 
 t3 = DockerOperator(api_version='1.19',
     command='/bin/sleep 30',
@@ -54,12 +58,6 @@ t3 = DockerOperator(api_version='1.19',
     dag=dag)
 
 
-t4 = BashOperator(
-    task_id='print_hello',
-    bash_command='echo "hello world!!!"',
-    dag=dag)
-
 
 t1.set_downstream(t2)
 t1.set_downstream(t3)
-t3.set_downstream(t4)
